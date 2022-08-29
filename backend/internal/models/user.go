8146fcf03fb1037f4787acd64f6acfd89870010a
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-08-29 20:12:51 +0800

    Created a function that creates a connection to the database.

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -13,16 +13,27 @@ type user struct {
 	Password string
 }
 
-// Queries the database for valid user credentials
-func QueryUser(username, password string) (u string, ifExists bool) {
-	user := &user{}
+// Creates a connection to the database
+func Connect() (db *sql.DB) {
 	db, err := sql.Open("sqlite3", "./internal/storage/database.db")
 	if err != nil {
 		log.Fatal().Msg(err.Error())
 	}
+	return
+}
+
+// Queries the database for valid user credentials
+func QueryUser(username, password string) (u string, ifExists bool) {
+	user := &user{}
+	db := Connect()
+
+	// db, err := sql.Open("sqlite3", "./internal/storage/database.db")
+	// if err != nil {
+	// 	log.Fatal().Msg(err.Error())
+	// }
 	stm, _ := db.Prepare("SELECT * FROM user WHERE username = ? AND password = ?")
 
-	err = stm.QueryRow(username, password).Scan(&user.ID, &user.Username, &user.Password)
+	err := stm.QueryRow(username, password).Scan(&user.ID, &user.Username, &user.Password)
 	log.Info().Msg(user.Username)
 	if err == sql.ErrNoRows {
 		return "", false
