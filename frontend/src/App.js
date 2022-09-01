1637c25b83afc9442f1e7e35901589368aef2c3c
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-09-01 18:09:12 +0800

    Remove test function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const test = async () => {
-  let username = "admin"
-  let password = "admin"
-  let response = await fetch('http://127.0.0.1:8080/api/v1/user/login', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    credentials: 'include',
-    body: JSON.stringify({
-        username,
-        password
-    })
-});
-
-    console.log(response.headers);
-
-    
-    response = await fetch('http://127.0.0.1:8080/api/v1/protected', {
-      credentials: 'include',
-  })
-  console.log(response)
-};
-
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" onLoad={test} />
+                <img src={logo} className="App-logo" alt="logo"/>
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
