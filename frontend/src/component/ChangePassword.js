92ddbc8658b1029abf40a58fa9a4bdeefbff1784
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-09-06 09:49:13 +0800

    Updated the ChangePassword.js

--- frontend/src/component/ChangePassword.js
+++ frontend/src/component/ChangePassword.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+    Alert,
     Button,
     Center,
     Container,
@@ -8,8 +9,12 @@ import {
     Title,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
-function ChangePassword() {
+import axios from "axios";
+import { IconAlertCircle } from "@tabler/icons";
 
+function ChangePassword() {
+    const [successStatus, setSuccessStatus] = useState();
+    const [failedStatus, setFailedStatus] = useState();
     const form = useForm({
         initialValues: {
             current_password: "",
@@ -22,13 +27,57 @@ function ChangePassword() {
             new_password: (value) =>
                 value.length < 12 ? "Must be more than 12 characters." : null,
             confirm_new_password: (value, values) =>
-                value !== values.new_password ? "The passwords you entered do not match." : null,
+                value !== values.new_password
+                    ? "The passwords you entered do not match."
+                    : null,
         },
     });
-
+    // Renders the flash messages
+    const FlashMessage = () => {
+        if (successStatus) {
+            return (
+                <Alert
+                    icon={<IconAlertCircle size={16} />}
+                    title="Done!"
+                    color="teal"
+                >
+                    Your password has been changed.
+                </Alert>
+            );
+        } if (failedStatus) {
+            return (
+                <Alert
+                    icon={<IconAlertCircle size={16} />}
+                    title="Bummer!"
+                    color="red"
+                >
+                    Something has went wrong while changing your password.
+                </Alert>
+            );
+        }
+    };
+    // Sends a POST request to the API.
     const changePasswordRequest = form.onSubmit((values) => {
-
-    })
+        axios
+            .post(
+                "http://localhost:8080/api/v1/user/change-password",
+                {
+                    current_password: values.current_password,
+                    new_password: values.new_password,
+                    confirm_new_password: values.confirm_new_password,
+                },
+                {
+                    withCredentials: true,
+                }
+            )
+            .then((res) => {
+                console.log(res.data);
+                setSuccessStatus(true);
+            })
+            .catch((err) => {
+                setFailedStatus(true);
+            });
+    });
     return (
         <div>
             <Container size={460} my={200}>
@@ -36,6 +85,7 @@ function ChangePassword() {
                     <Title order={1} mb="md" align="center">
                         Change Password
                     </Title>
+                    <FlashMessage />
                     <form onSubmit={changePasswordRequest}>
                         <PasswordInput
                             label="Current Password"
