6a332c9845eeaaf80579b1f082f96980b93060ed
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-09-26 09:00:00 +0800

    Added SQL functions

--- backend/internal/models/sshkeypair.go
+++ backend/internal/models/sshkeypair.go
@@ -1,8 +1,92 @@
 package models
 
+import (
+	_ "github.com/mattn/go-sqlite3"
+)
+
 type SSHKeyPair struct {
 	ID      int
-	label   string
-	pubKey  string
-	privKey string
+	Label   string
+	PubKey  string
+	PrivKey string
+}
+
+func (keypair *SSHKeyPair) QuerySSHKeyPair(id int) error {
+	db := connect()
+	stm, err := db.Prepare("SELECT * FROM sshpair WHERE id = ?")
+	if err != nil {
+		return err
+	}
+	err = stm.QueryRow(id).Scan(&keypair.ID, &keypair.Label, &keypair.PubKey, &keypair.PrivKey)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (keypair *SSHKeyPair) InsertSSHPairKey(label, pubKey, privKey string) error {
+	db := connect()
+	stm, err := db.Prepare("INSERT INTO sshpair (label, pub_key, priv_key) VALUES(?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	_, err = stm.Exec(label, pubKey, privKey)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (keypair *SSHKeyPair) QuerySSHKeyPairLabels() ([]string, error) {
+	db := connect()
+	labels := []string{}
+	rows, err := db.Query("SELECT label from sshpair")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		rows.Scan(&keypair.Label)
+		labels = append(labels, keypair.Label)
+	}
+	return labels, nil
+}
+
+func (keypair *SSHKeyPair) DeleteSSHKeyPair(id int) error {
+	db := connect()
+	err := keypair.QuerySSHKeyPair(id)
+	if err != nil {
+		return err
+	}
+	stm, err := db.Prepare("DELETE FROM sshpair WHERE id = ?")
+	if err != nil {
+		return err
+	}
+	_, err = stm.Exec(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (keypair *SSHKeyPair) UpdateSSHKeyPairLabel(id int, label string) error {
+	db := connect()
+	err := keypair.QuerySSHKeyPair(id)
+	if err != nil {
+		return err
+	}
+	stm, err := db.Prepare("UPDATE sshpair SET label = ? WHERE id = ?")
+	if err != nil {
+		return err
+	}
+	_, err = stm.Exec(label, id)
+	if err != nil {
+		return err
+	}
+	err = keypair.QuerySSHKeyPair(id)
+	if err != nil {
+		return nil
+	}
+	return nil
 }
