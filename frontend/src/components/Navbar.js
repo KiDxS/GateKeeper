8d413fb53f2847d80ebf70cd32e9d8aa9eb200ee
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-10-26 15:27:00 +0800

    feat: added the other links

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Heading, useColorModeValue, Flex, Box } from "@chakra-ui/react";
+import {
+    Heading,
+    useColorModeValue,
+    Flex,
+    Box,
+    Link,
+    Stack,
+} from "@chakra-ui/react";
 import ThemeModeSwitch from "./ThemeModeSwitch";
 
 const Navbar = () => {
@@ -9,12 +16,19 @@ const Navbar = () => {
             px={4}
             alignItems="center"
             justifyContent="space-between"
+            color={useColorModeValue("white")}
             bgColor={useColorModeValue("blackAlpha.900", "blackAlpha.600")}
         >
             <Box color={useColorModeValue("white")}>
                 <Heading size={{ base: "md", sm: "xl" }}>GateKeeper</Heading>
             </Box>
-            <ThemeModeSwitch />
+            <Stack direction="row" spacing={7} alignItems="center">
+                <Link href="/dashboard">Dashboard</Link>
+                <Link href="/change-password">Change Password</Link>
+                <Link href="">Logout</Link>
+
+                <ThemeModeSwitch />
+            </Stack>
         </Flex>
     );
 };
