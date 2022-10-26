8961be7b81f9e202d8196cbde7d4d933b02bea5b
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-10-26 14:10:44 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import theme from "./theme";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeScript } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <ChakraProvider>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
     </ChakraProvider>
 );
