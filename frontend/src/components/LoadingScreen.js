122b8aaf5c5437face810e0d15c4adf01266fcdd
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-10-26 15:25:21 +0800

    feat: added a loading screen

---
+++ frontend/src/components/LoadingScreen.js
@@ -0,0 +1,31 @@
+import { React, useEffect, useState } from "react";
+import { Spinner, Flex } from "@chakra-ui/react";
+const LoadingScreen = (props) => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1500);
+    }, []);
+    return (
+        <>
+            {loading ? (
+                <Flex
+                    alignItems="center"
+                    justifyContent="center"
+                    minH="100vh"
+                    textAlign="center"
+                >
+                    <Spinner
+                        color="body"
+                        bgColor="bg"
+                        verticalAlign="middle"
+                        size="xl"
+                    ></Spinner>
+                </Flex>
+
+            ) : props.children}
+        </>
+    );
+};
+
+export default LoadingScreen;
