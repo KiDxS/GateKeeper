c97bbed8b2e0aaf8a0410e3f17c83ca700631bda
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-10-30 19:51:08 +0800

    feat: added a feature that redirects us to the dashboard if user is logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { React, useRef, useState } from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 import LoginPage from "./pages/LoginPage";
+import { useCookies } from "react-cookie";
 
 const App = () => {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [cookies] = useCookies();
+    if (cookies.authToken && cookies.authToken.length !== 0) {
+        setLoggedIn(true);
+    }
     return (
         <Router>
             <Routes>
-                <Route exact path="/" element={<LoginPage />}></Route>
-                <Route exact path="/dashboard" element=""></Route>
+                <Route
+                    exact
+                    path="/"
+                    element={
+                        loggedIn ? <Navigate to="/dashboard" /> : <LoginPage />
+                    }
+                ></Route>
+                <Route exact path="/dashboard" element={<Dashboard />}></Route>
                 <Route exact path="/change-password" element=""></Route>
             </Routes>
         </Router>
