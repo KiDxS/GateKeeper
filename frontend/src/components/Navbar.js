ed09f8b14709fc8e5065f36a7211b4a782dd22a2
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-10-30 19:51:48 +0800

    feat: created a function that will logout the user.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,10 +6,27 @@ import {
     Box,
     Link,
     Stack,
+    Button,
 } from "@chakra-ui/react";
 import ThemeModeSwitch from "./ThemeModeSwitch";
+import { fetchData } from "../utils/fetchData";
+import { useNavigate } from "react-router-dom";
+import AuthProvider from "./AuthProvider";
 
 const Navbar = () => {
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        try {
+            const options = { withCredentials: true };
+            const response = await fetchData(
+                "http://127.0.0.1:8080/api/v1/user/logout",
+                options
+            );
+            navigate("/");
+        } catch (err) {
+            console.log(err);
+        }
+    };
     return (
         <Flex
             h={16}
@@ -23,9 +40,13 @@ const Navbar = () => {
                 <Heading size={{ base: "md", sm: "xl" }}>GateKeeper</Heading>
             </Box>
             <Stack direction="row" spacing={7} alignItems="center">
-                <Link href="/dashboard">Dashboard</Link>
-                <Link href="/change-password">Change Password</Link>
-                <Link href="">Logout</Link>
+                <AuthProvider>
+                    <Link href="/dashboard">Dashboard</Link>
+                    <Link href="/change-password">Change Password</Link>
+                    <Button variant="link" onClick={handleLogout} color="body">
+                        Logout
+                    </Button>
+                </AuthProvider>
 
                 <ThemeModeSwitch />
             </Stack>
