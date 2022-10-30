e2cdb0d43d9d1ff9e66c83e921135da5bd316d30
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-10-30 19:57:54 +0800

    refactor: removed loading screen

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -16,8 +16,10 @@ import Navbar from "../components/Navbar";
 import LoadingScreen from "../components/LoadingScreen";
 import { postData } from "../utils/postData";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+    const navigate = useNavigate();
     const {
         register,
         handleSubmit,
@@ -32,10 +34,16 @@ const LoginPage = () => {
     const handleLogin = async (data) => {
         try {
             const url = "http://127.0.0.1:8080/api/v1/user/login";
-            const response = await postData(url, data, null);
-            console.log(response.status);
+            // eslint-disable-next-line no-unused-vars
+            const options = { withCredentials: true }
+            const response = await postData(url, data, options);
             loginStatus.success.current = true;
+            navigate("/dashboard");
         } catch (err) {
+            if (err.code === "ERR_NETWORK") {
+                loginStatus.error.current = true;
+                return;
+            }
             switch (err.response.status) {
                 case 401:
                     loginStatus.wrong_creds.current = true;
@@ -73,73 +81,70 @@ const LoginPage = () => {
         }
     };
     return (
-        <LoadingScreen>
-            <Box>
-                <Box>
-                    <Navbar />
+        <>
+            <Navbar />
+
+            <Container>
+                <Stack spacing={{ base: 2, sm: 4 }} mt="12">
+                    <Heading
+                        size={{ base: "2xl", sm: "4xl" }}
+                        textAlign="center"
+                    >
+                        GateKeeper
+                    </Heading>
+                    <Heading size={{ base: "sm", sm: "md" }} textAlign="center">
+                        Login to your account
+                    </Heading>
+                </Stack>
+                <Box my="7" borderWidth="1px" borderRadius="md" boxShadow="2xl">
+                    <form onSubmit={handleSubmit(handleLogin)}>
+                        <Stack spacing="4" p={{ base: 10, sm: 20 }}>
+                            {renderAlert()}
+                            <FormControl isInvalid={errors.username}>
+                                <FormLabel htmlFor="username" fontWeight="bold">
+                                    Username
+                                </FormLabel>
+                                <Input
+                                    id="username"
+                                    type="text"
+                                    placeholder="Username"
+                                    {...register("username", {
+                                        required: "This field is required.",
+                                    })}
+                                />
+                                <FormErrorMessage>
+                                    {errors.username && errors.username.message}
+                                </FormErrorMessage>
+                            </FormControl>
+                            <FormControl isInvalid={errors.password}>
+                                <FormLabel htmlFor="password" fontWeight="bold">
+                                    Password
+                                </FormLabel>
+                                <Input
+                                    type="password"
+                                    placeholder="Password"
+                                    {...register("password", {
+                                        required: "This field is required.",
+                                    })}
+                                />
+                                <FormErrorMessage>
+                                    {errors.password && errors.password.message}
+                                </FormErrorMessage>
+                            </FormControl>
+                            <Button
+                                type="submit"
+                                colorScheme="blue"
+                                variant="solid"
+                                size={{ base: "md", sm: "lg" }}
+                                isLoading={isSubmitting}
+                            >
+                                Login
+                            </Button>
+                        </Stack>
+                    </form>
                 </Box>
-                <Container>
-                    <Stack spacing={{ base: 2, sm: 4 }} mt="12">
-                        <Heading
-                            size={{ base: "2xl", sm: "4xl" }}
-                            textAlign="center"
-                        >
-                            GateKeeper
-                        </Heading>
-                        <Heading size={{ base: "sm", sm: "md" }} textAlign="center">
-                            Login to your account
-                        </Heading>
-                    </Stack>
-                    <Box my="7" borderWidth="1px" borderRadius="md" boxShadow="2xl">
-                        <form onSubmit={handleSubmit(handleLogin)}>
-                            <Stack spacing="4" p={{ base: 10, sm: 20 }}>
-                                {renderAlert()}
-                                <FormControl isInvalid={errors.username}>
-                                    <FormLabel htmlFor="username" fontWeight="bold">
-                                        Username
-                                    </FormLabel>
-                                    <Input
-                                        id="username"
-                                        type="text"
-                                        placeholder="Username"
-                                        {...register("username", {
-                                            required: "This field is required.",
-                                        })}
-                                    />
-                                    <FormErrorMessage>
-                                        {errors.username && errors.username.message}
-                                    </FormErrorMessage>
-                                </FormControl>
-                                <FormControl isInvalid={errors.password}>
-                                    <FormLabel htmlFor="password" fontWeight="bold">
-                                        Password
-                                    </FormLabel>
-                                    <Input
-                                        type="password"
-                                        placeholder="Password"
-                                        {...register("password", {
-                                            required: "This field is required.",
-                                        })}
-                                    />
-                                    <FormErrorMessage>
-                                        {errors.password && errors.password.message}
-                                    </FormErrorMessage>
-                                </FormControl>
-                                <Button
-                                    type="submit"
-                                    colorScheme="blue"
-                                    variant="solid"
-                                    size={{ base: "md", sm: "lg" }}
-                                    isLoading={isSubmitting}
-                                >
-                                    Login
-                                </Button>
-                            </Stack>
-                        </form>
-                    </Box>
-                </Container>
-            </Box>
-        </LoadingScreen>
+            </Container>
+        </>
     );
 };
 
