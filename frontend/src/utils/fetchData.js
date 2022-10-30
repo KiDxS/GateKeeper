253bd006639c89f00c3bad6546ad8c3a8d6c2d16
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-10-30 19:58:47 +0800

    refactor: return the response rather than the data

--- frontend/src/utils/fetchData.js
+++ frontend/src/utils/fetchData.js
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export const fetchData = async (url, options) => {
     const response = await axios.get(url, options);
-    const data = response.json();
-    return data;
+    return response;
 };
