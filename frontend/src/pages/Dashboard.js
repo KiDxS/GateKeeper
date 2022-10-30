95992a44b643a74c81936bd9f9591dccdc8ad5f6
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-10-30 19:52:24 +0800

    feat: created component "Dashboard"

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,20 @@
+import { React } from "react";
+import { Box, Stack, Heading, Text, Container } from "@chakra-ui/react";
+import LoadingScreen from "../components/LoadingScreen";
+import Navbar from "../components/Navbar";
+import AuthProvider from "../components/AuthProvider";
+
+const Dashboard = () => {
+    return (
+        <LoadingScreen>
+            <AuthProvider>
+                <Navbar />
+                <Container>
+                    <Heading>Dashboard!!</Heading>
+                </Container>
+            </AuthProvider>
+        </LoadingScreen>
+    );
+};
+
+export default Dashboard;
