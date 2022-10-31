99645a2d20e29ac0684c588a2252738546dbad53
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-10-31 13:44:16 +0800

    docs: added a more detailed description to the fields.

--- backend/internal/web/handlers/auth/fields.go
+++ backend/internal/web/handlers/auth/fields.go
@@ -1,13 +1,13 @@
 package auth
 
-// Field struct for handling change password requests
+// ChangePasswordFields is a struct that is used to store the user's request to change their password.
 type ChangePasswordFields struct {
 	CurrentPassword    string `json:"current_password" validate:"required"`
 	NewPassword        string `json:"new_password" validate:"required,min=12"`
 	ConfirmNewPassword string `json:"confirm_password" validate:"required,min=12,eqfield=NewPassword"`
 }
 
-// Field struct for handling the login requests
+// LoginFields struct is a struct that is used to store the user's request to login to their account.
 type LoginFields struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
--- backend/internal/web/handlers/ssh/fields.go
+++ backend/internal/web/handlers/ssh/fields.go
@@ -1,6 +1,6 @@
 package ssh
 
-// Field struct for handling SSH generation requests
+// SSHGenerationFields is a struct that is used to store the values coming from the user's request to generate a new SSH key pair.
 type SSHGenerationFields struct {
 	Label    string `json:"label" validate:"required"`
 	Password string `json:"password"`
