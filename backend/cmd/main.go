3c1935a387ce73f2f7356c55372b96689076efb8
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-10-31 10:06:37 +0800

    security: added HTTP timeouts to mitigate slowloris

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/KiDxS/GateKeeper/internal/web"
 )
 
 func main() {
+	server := &http.Server{
+		Addr:              ":8080",
+		ReadHeaderTimeout: 3 * time.Second,
+		Handler:           web.Routes(),
+	}
 	log.Println("Server is listening on port 8080")
-	http.ListenAndServe(":8080", web.Routes())
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
