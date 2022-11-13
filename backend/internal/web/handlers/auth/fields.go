653129f46cdb0e607f2a96cdceddda0704a5f389
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-11-13 13:23:14 +0800

    refactor: confirm_password to confirm_new_password field

--- backend/internal/web/handlers/auth/fields.go
+++ backend/internal/web/handlers/auth/fields.go
@@ -4,7 +4,7 @@ package auth
 type ChangePasswordFields struct {
 	CurrentPassword    string `json:"current_password" validate:"required"`
 	NewPassword        string `json:"new_password" validate:"required,min=12"`
-	ConfirmNewPassword string `json:"confirm_password" validate:"required,min=12,eqfield=NewPassword"`
+	ConfirmNewPassword string `json:"confirm_new_password" validate:"required,min=12,eqfield=NewPassword"`
 }
 
 // LoginFields struct is a struct that is used to store the user's request to login to their account.
