7e009ab3feb396c4052af52416667b1c6c565bac
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-11-14 21:27:08 +0800

    feat: render data from the api

---
+++ frontend/src/components/DeleteDialog.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+    Button,
+    useDisclosure,
+} from "@chakra-ui/react";
+const DeleteDialog = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const cancelRef = React.useRef();
+    return (
+        <>
+            <Button onClick={onOpen} variant="solid" colorScheme="red" >
+                Delete
+            </Button>
+
+            <AlertDialog
+                motionPreset="slideInBottom"
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                            Delete SSH keypair
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>
+                            Are you sure? You can't undo this action afterwards.
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button colorScheme="red" onClick={onClose} ml={3}>
+                                Delete
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+        </>
+    );
+};
+
+export default DeleteDialog;
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,17 +1,103 @@
-import { React } from "react";
-import { Box, Stack, Heading, Text, Container } from "@chakra-ui/react";
+import { React, useEffect, useState } from "react";
+import {
+    Stack,
+    Container,
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    TableContainer,
+    Button,
+} from "@chakra-ui/react";
+
 import LoadingScreen from "../components/LoadingScreen";
 import Navbar from "../components/Navbar";
 import AuthProvider from "../components/AuthProvider";
+import DeleteDialog from "../components/DeleteDialog";
+import { fetchData } from "../utils/fetchData";
 
 // Dashboard page
 const Dashboard = () => {
+    const [keypairLabel, setKeypairLabel] = useState([]);
+    useEffect(() => {
+        async function fetchKeypairLabels() {
+            try {
+                const options = { withCredentials: true };
+                const response = await fetchData(
+                    "http://127.0.0.1:8080/api/v1/key",
+                    options
+                );
+                const json = await response.data;
+                setKeypairLabel(json.data);
+            } catch (err) {}
+        }
+        fetchKeypairLabels();
+    }, []);
+
+    const labels = keypairLabel.map((label, id) => {
+        return (
+            <Tr>
+                <Td>{label}</Td>
+                <Td>
+                    <Stack spacing={2} direction="row">
+                        <Button variant="solid" colorScheme="blue">
+                            View
+                        </Button>
+                        <DeleteDialog />
+                    </Stack>
+                </Td>
+            </Tr>
+        );
+    });
+
     return (
         <LoadingScreen>
             <AuthProvider>
                 <Navbar />
-                <Container>
-                    <Heading>Dashboard!!</Heading>
+                <Container maxW="6xl" mt={8}>
+                    <TableContainer>
+                        <Table size="lg" variant="simple">
+                            <Thead>
+                                <Tr>
+                                    <Th>Label</Th>
+                                    <Th>Actions</Th>
+                                </Tr>
+                            </Thead>
+                            <Tbody>
+                                {labels}
+                                {/* <Tr>
+                                    <Td>This is just a test</Td>
+                                    <Td>
+                                        <Stack spacing={2} direction="row">
+                                            <Button
+                                                variant="solid"
+                                                colorScheme="blue"
+                                            >
+                                                View
+                                            </Button>
+                                            <DeleteDialog />
+                                        </Stack>
+                                    </Td>
+                                </Tr>
+                                <Tr>
+                                    <Td>This is just a test</Td>
+                                    <Td>
+                                        <Stack spacing={2} direction="row">
+                                            <Button
+                                                variant="solid"
+                                                colorScheme="blue"
+                                            >
+                                                View
+                                            </Button>
+                                            <DeleteDialog />
+                                        </Stack>
+                                    </Td>
+                                </Tr>  */}
+                            </Tbody>
+                        </Table>
+                    </TableContainer>
                 </Container>
             </AuthProvider>
         </LoadingScreen>
