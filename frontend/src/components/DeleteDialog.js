132e864d82dcea50b8e77034febfac9a19f45005
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-11-20 10:15:18 +0800

    feat: deletes the label

--- frontend/src/components/DeleteDialog.js
+++ frontend/src/components/DeleteDialog.js
@@ -9,12 +9,28 @@ import {
     Button,
     useDisclosure,
 } from "@chakra-ui/react";
-const DeleteDialog = () => {
+import axios from "axios";
+const DeleteDialog = (props) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const cancelRef = React.useRef();
+
+    const onDelete = async () => {
+       try {
+        const id = props.labelID;
+        const options = { withCredentials: true };
+        const url = `http://127.0.0.1:8080/api/v1/key/${id}`
+        const response = await axios.delete(url, options);
+        onClose();
+       } catch (err) {
+
+       }
+    };
+    // const onTest = () => {
+    //     console.log(props.labelID);
+    // }
     return (
         <>
-            <Button onClick={onOpen} variant="solid" colorScheme="red" >
+            <Button onClick={onOpen} variant="solid" colorScheme="red">
                 Delete
             </Button>
 
@@ -38,7 +54,7 @@ const DeleteDialog = () => {
                             <Button ref={cancelRef} onClick={onClose}>
                                 Cancel
                             </Button>
-                            <Button colorScheme="red" onClick={onClose} ml={3}>
+                            <Button colorScheme="red" onClick={onDelete} ml={3}>
                                 Delete
                             </Button>
                         </AlertDialogFooter>
