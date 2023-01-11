ebf81285f7283578b83a6c0d72da5657f38522f7
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2023-01-11 10:18:27 +0800

    Update Dashboard.js

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import { React } from "react";
 import {
     Stack,
     Container,
@@ -17,26 +17,27 @@ import Navbar from "../components/Navbar";
 import AuthProvider from "../components/AuthProvider";
 import DeleteDialog from "../components/DeleteDialog";
 import { fetchData } from "../utils/fetchData";
+import { useQuery } from "@tanstack/react-query";
 
 // Dashboard page
 const Dashboard = () => {
-    const [keypairs, setKeypairs] = useState([]);
-    useEffect(() => {
-        async function fetchKeypairLabels() {
-            try {
-                const options = { withCredentials: true };
-                const response = await fetchData(
-                    "http://127.0.0.1:8080/api/v1/key",
-                    options
-                );
-                const json = await response.data;
-                setKeypairs(json.data);
-            } catch (err) {}
-        }
-        fetchKeypairLabels();
-    }, []);
+    const fetchKeypairs = async () => {
+        try {
+            const options = { withCredentials: true };
+            const response = await fetchData(
+                "http://127.0.0.1:8080/api/v1/key",
+                options
+            );
+            const json = await response.data;
+            // console.log(json);
+            return json.data;
+        } catch (err) {}
+    };
+    const { isLoading, data } = useQuery(["keypairs"], fetchKeypairs);
 
-    const labels = keypairs.map((keypair) => {
+    if (isLoading) return "Loading";
+
+    const labels = data.map((keypair) => {
         return (
             <Tr>
                 <Td>{keypair.label}</Td>
@@ -65,9 +66,7 @@ const Dashboard = () => {
                                     <Th>Actions</Th>
                                 </Tr>
                             </Thead>
-                            <Tbody>
-                                {labels}
-                            </Tbody>
+                            <Tbody>{labels}</Tbody>
                         </Table>
                     </TableContainer>
                 </Container>
