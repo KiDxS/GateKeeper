9fe8e2339cb4e2033d7e05e985ed33d9a6f78c3f
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2023-01-11 10:19:43 +0800

    Update LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -44,7 +44,7 @@ const LoginPage = () => {
         try {
             const url = "http://127.0.0.1:8080/api/v1/user/login";
             const options = { withCredentials: true };
-            const response = await postData(url, data, options);
+            await postData(url, data, options);
             status.success.current = true;
             navigate("/dashboard");
         } catch (err) {
