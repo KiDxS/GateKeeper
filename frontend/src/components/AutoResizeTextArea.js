ba6491b4a5aa6b39b6103c9bf2f9dc9b1e664180
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2023-01-12 19:49:16 +0800

    Create AutoResizeTextArea.js

---
+++ frontend/src/components/AutoResizeTextArea.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ResizeTextArea from "react-textarea-autosize";
+import { Textarea } from "@chakra-ui/react";
+
+
+const AutoResizeTextArea = React.forwardRef((props, ref) => {
+    return (
+        <Textarea
+            size="lg"
+            maxH="400px"
+            ref={ref}
+            minRows={1}
+            as={ResizeTextArea}
+            {...props}
+            resize="none"
+            isDisabled
+        ></Textarea>
+    );
+});
+
+export default AutoResizeTextArea;
