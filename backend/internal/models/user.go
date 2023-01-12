8320680fe19ceef723d647f909c045cee34fbcb4
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2023-01-12 19:48:43 +0800

    added bcrypt hashing

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 
 	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -12,18 +13,28 @@ type User struct {
 	Password string
 }
 
+func CheckHashedPassword(hashedPassword, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		return false
+	}
+	return true
+}
+
 // QueryUser is a function that is used to query the database for an user. This function takes two arguments which are "username" and "password".
 func (user *User) QueryUser(username, password string) (string, bool) {
 	db := connect()
 
-	stm, _ := db.Prepare("SELECT * FROM user WHERE username = ? AND password = ?")
+	stm, _ := db.Prepare("SELECT id, username, password FROM user WHERE username = ?")
 
-	err := stm.QueryRow(username, password).Scan(&user.ID, &user.Username, &user.Password)
+	err := stm.QueryRow(username).Scan(&user.ID, &user.Username, &user.Password)
 	if err == sql.ErrNoRows {
 		return "", false
 	}
+	if !CheckHashedPassword(user.Password, password) {
+		return "", false
+	}
 	return user.Username, true
-
 }
 
 // ChangeUserPassword is a function that is used to change the user's password in the database. This function takes three arguments which are "username", "currentPassword", and "newPassword".
